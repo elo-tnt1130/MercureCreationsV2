09c7b8f3bd1364170f3d276a3a6f8517d1f9ee7d
elo-tnt1130 <elo.caille@gmail.com>
2025-10-08 10:03:13 +0200

    flag_maintenance

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
+if (file_exists(__DIR__ . '/../maintenance.flag')) {
+    $_SERVER['REQUEST_URI'] = '/maintenance';
+    //     require __DIR__.'/../src/MaintenanceKernel.php';
+    //     exit;
+}
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
